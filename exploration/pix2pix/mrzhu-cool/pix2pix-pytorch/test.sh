ec50980f4375de90f9fdfefaaec3192c71a3f848
Lucien Rae Gentil <lucienraegentil@gmail.com>
2021-09-12 09:44:29 +1000

    Ran experiments of pix2pix on Tufts dataset

--- exploration/pix2pix/mrzhu-cool/pix2pix-pytorch/test.sh
+++ exploration/pix2pix/mrzhu-cool/pix2pix-pytorch/test.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
+python test.py --dataset tufts_face_rgb_ir --nepochs 200 --cuda
+=======
 python test.py --dataset tufts_face_rgb_ir --nepochs 4 --cuda
+>>>>>>> 6af543522ddc5fef55539dee4f4ba6bc23d59b9c
